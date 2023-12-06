03b0571aa49f909fb4d76070e88bc77fa5827c93
George Salosin <gsalosin@gmail.com>
2023-12-06 07:59:55 +0700

    Common functions moved to utility

--- file.py
+++ file.py
@@ -1,27 +1,20 @@
 
 from pygame import *
 
-screen = display.set_mode((1920, 1080), RESIZABLE)
 
-gx = -12288
-gy = -5120
-mouse.get_pos()
-background = image.load(f'Content/Map/Overcast/Overcast_UltraLow.jpg')
-background = transform.scale(background, [background.get_width()*0.5, background.get_height()*0.5])
-event.set_grab(True)
-def lock_mouse(gx, gy):
-    px = 400
-    py = 400
-    mouse_pos = (px, py)
-    rel = mouse.get_rel()
-    gx += rel[0]
-    gy += rel[1]
-    if mouse.get_pos()[0] < mouse_pos[0] - 100 \
-            or mouse.get_pos()[0] > mouse_pos[0] + 100 \
-            or mouse.get_pos()[1] < mouse_pos[1] - 100 \
-            or mouse.get_pos()[1] > mouse_pos[1] + 100:
-        mouse.set_pos([px, py])
-    return gx, gy
+screen = display.set_mode((800, 400), RESIZABLE)
+gx = 0
+gy = 0
+bg = image.load(f'bg.png')
+aspect = screen.get_width()/bg.get_width()
+bg = transform.scale(bg, [bg.get_width()* aspect, bg.get_height()*aspect])
+clock = time.Clock()
+FPS = 60
+def bg_scroll(bg, gx, gy):
+    inst01 = bg
+    inst02 = bg
+    inst03 = bg
+
 run = True
 while run:
 
@@ -31,10 +24,7 @@ while run:
         elif e.type == KEYDOWN:
             if e.key == K_ESCAPE:
                 run = False
-        elif e.type == MOUSEMOTION:
-            gx,gy = lock_mouse(gx, gy)
-
-    screen.blit(background, (gx,gy))
+    clock.tick(FPS)
+    screen.blit(bg, (gx,gy))
     display.flip()
-
 
